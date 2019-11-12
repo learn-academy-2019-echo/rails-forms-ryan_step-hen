cbe18f8ff5075cfc8950e9e59da7c6777cdc09a8
Matthew Clark <winescout@gmail.com>
2019-11-12 14:32:04 -0800

    adds controller actions and views

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,32 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     #render "show.html.erb"
   end
+
+  def new
+    #render "new.html.erb"
+  end
+
+  def create
+    @post = BlogPost.new(
+      title: params[:title],
+      content: params[:content]
+    )
+
+    if false && @post.save
+      redirect_to @post
+    else
+      render action: :new
+    end
+  end
+
+  def delete
+    @post = BlogPost.find(params[:id])
+    if @post.destroy
+      redirect_to blog_posts_path
+    else
+      redirect_to blog_post_path(@post)
+    end 
+
+  end
+
 end
